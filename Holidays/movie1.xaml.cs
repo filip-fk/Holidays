1c998454fe092c6bb8d3e372480968fd181bf692
Philip Kalapish <filipkalapish@outlook.com>
2017-01-25 11:21:43 +0200

    m&g

--- Holidays/movie1.xaml.cs
+++ Holidays/movie1.xaml.cs
@@ -7,6 +7,7 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Media.Core;
 using Windows.Storage;
+using Windows.UI.Input.Inking;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -28,7 +29,96 @@ namespace Holidays
         public movie1()
         {
             this.InitializeComponent();
+            // Set supported inking device types.
+            inkCanvas.InkPresenter.InputDeviceTypes =
+                Windows.UI.Core.CoreInputDeviceTypes.Mouse |
+                Windows.UI.Core.CoreInputDeviceTypes.Touch |
+                Windows.UI.Core.CoreInputDeviceTypes.Pen;
+
+            // Set initial ink stroke attributes.
+            InkDrawingAttributes drawingAttributes = new InkDrawingAttributes();
+            drawingAttributes.Color = Windows.UI.Colors.Black;
+            drawingAttributes.IgnorePressure = false;
+            drawingAttributes.FitToCurve = true;
+            inkCanvas.InkPresenter.UpdateDefaultDrawingAttributes(drawingAttributes);
+
+            // Listen for button click to initiate recognition.
+            recognize.Click += Recognize_Click;
         }
+
+        string newstr = "";
+
+        // Handle button click to initiate recognition.
+        private async void Recognize_Click(object sender, RoutedEventArgs e)
+        {
+            // Get all strokes on the InkCanvas.
+            IReadOnlyList<InkStroke> currentStrokes = inkCanvas.InkPresenter.StrokeContainer.GetStrokes();
+
+            
+            // Ensure an ink stroke is present.
+            if (currentStrokes.Count > 0)
+            {
+                // Create a manager for the InkRecognizer object
+                // used in handwriting recognition.
+                InkRecognizerContainer inkRecognizerContainer =
+                    new InkRecognizerContainer();
+
+                // inkRecognizerContainer is null if a recognition engine is not available.
+                if (!(inkRecognizerContainer == null))
+                {
+                    // Recognize all ink strokes on the ink canvas.
+                    IReadOnlyList<InkRecognitionResult> recognitionResults =
+                        await inkRecognizerContainer.RecognizeAsync(
+                            inkCanvas.InkPresenter.StrokeContainer,
+                            InkRecognitionTarget.All);
+                    // Process and display the recognition results.
+                    if (recognitionResults.Count > 0)
+                    {
+                        //string str = "Recognition result\n";
+                        // Iterate through the recognition results.
+                        foreach (var result in recognitionResults)
+                        {
+                            // Get all recognition candidates from each recognition result.
+                            IReadOnlyList<string> candidates = result.GetTextCandidates();
+                            //str += "Candidates: " + candidates.Count.ToString() + "\n";
+                            foreach (string candidate in candidates)
+                            {
+                                Button ncan = new Button();
+                                ncan.Content = candidate;
+                                ncan.Click += Ncan_Click;
+                                newstr += ncan.Content + " ";
+                                btnholder.Children.Add(ncan);
+                                //str += candidate + " ";
+                            }
+                        }
+                        // Display the recognition candidates.
+                        //recognitionResult.Text = str;
+                        // Clear the ink canvas once recognition is complete.
+                        inkCanvas.InkPresenter.StrokeContainer.Clear();
+                    }
+                    else
+                    {
+                        //recognitionResult.Text = "No recognition results.";
+                    }
+                }
+                else
+                {
+                    Windows.UI.Popups.MessageDialog messageDialog = new Windows.UI.Popups.MessageDialog("You must install handwriting recognition engine.");
+                    await messageDialog.ShowAsync();
+                }
+            }
+            else
+            {
+                //recognitionResult.Text = "No ink strokes to recognize.";
+            }
+        }
+
+        private void Ncan_Click(object sender, RoutedEventArgs e)
+        {
+            recognitionResult.Text = newstr;
+            
+        }
+
         /*
         private void RemoveButton_Click(object sender, RoutedEventArgs e)
         {
--- Holidays/wthgrid.xaml.cs
+++ Holidays/wthgrid.xaml.cs
@@ -32,30 +32,50 @@ namespace Holidays
             var nowm = updt.Minute;
             update.Text = "Updated as of " + $"{nowh}" + ":" + $"{nowm}" + " PM";
         }
-        
+
         public async void Button_Click()
         {
             var position = await LocationManager.GetPosition();
 
             //ResultTextBlock.Text = $"{position.Coordinate.Latitude}" +", "+$"{position.Coordinate.Longitude}";
 
-            
+
             RootObject myWeather = await OpenWeatherMapProxy.GetWeather(position.Coordinate.Latitude, position.Coordinate.Longitude);
 
             string icon = String.Format("ms-appx:///Assets/Weather/{0}.png", myWeather.list[0].weather[0].icon);
             ResultImage1.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
-            
+
             degreesmxd1.Text = ((int)myWeather.list[0].main.temp_max).ToString();
             comm1.Text = myWeather.list[0].weather[0].description;
             place.Text = myWeather.city.name;
-            windspeed.Text ="  "+ ((int)myWeather.list[0].wind.speed).ToString() + " km/h";
+            windspeed.Text = "  " + ((int)myWeather.list[0].wind.speed).ToString() + " km/h";
             humidity.Text = "  " + (myWeather.list[0].main.humidity).ToString() + " %";
             Preasure.Text = "  " + ((int)myWeather.list[0].main.pressure).ToString() + " mb";
-            feelslike.Text = ((int)myWeather.list[0].main.temp_max-5).ToString();
-            dewpoint.Text = ((int)myWeather.list[0].main.temp_max-2).ToString();
+            feelslike.Text = ((int)myWeather.list[0].main.temp_max - 5).ToString();
+            dewpoint.Text = ((int)myWeather.list[0].main.temp_max - 2).ToString();
             visibility.Text = "  " + "5" + " km";
 
             //degreesmnd1.Text = "  " + ((((int)myWeather.list[0].main.temp_max*9)/5+32).ToString());
+        }
+
+        private async void ftoc_ctof(object sender, RoutedEventArgs e)
+        {
+
+            var position = await LocationManager.GetPosition();
+            RootObject myWeather = await OpenWeatherMapProxy.GetWeather(position.Coordinate.Latitude, position.Coordinate.Longitude);
+            if (nowt.Text == "  C°")
+            {
+                nowt.Text = "  F°";
+                couldbt.Content = "  C°";
+                degreesmxd1.Text = "  " + ((((int)myWeather.list[0].main.temp_max * 9) / 5 + 32).ToString());
+            }
+
+            else
+            {
+                nowt.Text = "  C°";
+                couldbt.Content = "  F°";
+                degreesmxd1.Text = ((int)myWeather.list[0].main.temp_max).ToString();
+            }
         }
     }
 }
